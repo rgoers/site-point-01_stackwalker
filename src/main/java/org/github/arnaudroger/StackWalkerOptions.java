41cba27dbb510403bf68234bd6553f10d25d29ff
Arnaud Roger <arnaud.roger@gmail.com>
2017-02-07 09:57:00 +0000

    rename methods

--- src/main/java/org/github/arnaudroger/StackWalkerOptions.java
+++ src/main/java/org/github/arnaudroger/StackWalkerOptions.java
@@ -4,26 +4,26 @@ import java.util.Arrays;
 
 public class StackWalkerOptions {
 
-    public static void method1(Runnable r) throws Exception {
-        StackWalkerOptions.class.getMethod("method2", Runnable.class).invoke(null, r);
+    public static void delegateViaReflection(Runnable task) throws Exception {
+        StackWalkerOptions.class.getMethod("runTask", Runnable.class).invoke(null, task);
     }
 
-    public static void method2(Runnable r) {
+    public static void runTask(Runnable r) {
         r.run();
     }
 
     public static void main(String[] args) throws Exception {
         System.out.println("---- Default StackWalker----");
-        method1(() -> StackWalker.getInstance().forEach(System.out::println));
+        delegateViaReflection(() -> StackWalker.getInstance().forEach(System.out::println));
 
         System.out.println("---- SHOW_REFLECT_FRAMES StackWalker----");
-        method1(() -> StackWalker.getInstance(StackWalker.Option.SHOW_REFLECT_FRAMES).forEach(System.out::println));
+        delegateViaReflection(() -> StackWalker.getInstance(StackWalker.Option.SHOW_REFLECT_FRAMES).forEach(System.out::println));
 
         System.out.println("---- SHOW_HIDDEN_FRAMES StackWalker----");
-        method1(() -> StackWalker.getInstance(StackWalker.Option.SHOW_HIDDEN_FRAMES).forEach(System.out::println));
+        delegateViaReflection(() -> StackWalker.getInstance(StackWalker.Option.SHOW_HIDDEN_FRAMES).forEach(System.out::println));
 
         System.out.println("---- Exception ---");
-        method1(() -> Arrays.stream(new Throwable().getStackTrace()).forEach(System.out::println));
+        delegateViaReflection(() -> Arrays.stream(new Throwable().getStackTrace()).forEach(System.out::println));
     }
 
 }
